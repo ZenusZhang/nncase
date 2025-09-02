5cf3b64e8ce6a147ecf96edc7700a72a387f8cf2
huochenghai <huochenghai@canaan-creative.com>
2025-09-02 09:57:17 +0800

    optimize pad using copy

--- ntt/include/nncase/ntt/kernels/pad.h
+++ ntt/include/nncase/ntt/kernels/pad.h
@@ -21,9 +21,10 @@
 namespace nncase::ntt {
 namespace pad_detail {
 template <Tensor TIn, Tensor TOut, Paddings TPaddings, ScalarOrVector TElem>
-void pad_impl(const TIn &input, TOut &output, const TPaddings &paddings,
-              const TElem &pad_value) {
+void naive_apply(const TIn &input, TOut &output, const TPaddings &paddings,
+                 const TElem &pad_value) {
     constexpr auto rank = TIn::rank();
+    using element_type = typename TIn::element_type;
     ntt::apply(output.shape(), [&](auto out_index) {
         bool dopad = false;
         const auto in_index = generate_shape<rank>([&](auto i) {
@@ -33,12 +34,67 @@ void pad_impl(const TIn &input, TOut &output, const TPaddings &paddings,
             }
             return in_dim;
         });
+        auto addr_output =
+            reinterpret_cast<element_type *>(&(output(out_index)));
         if (dopad) {
-            output(out_index) = (typename TIn::element_type)pad_value;
+            ntt::u_unary(ntt::ops::copy<element_type>{}, &pad_value, 1,
+                         addr_output, 1, 1);
         } else {
-            output(out_index) = input(in_index);
+            auto addr_input =
+                reinterpret_cast<const element_type *>(&(input(in_index)));
+            ntt::u_unary(ntt::ops::copy<element_type>{}, addr_input, 1,
+                         addr_output, 1, 1);
         }
     });
+}
+
+template <Tensor TIn, Tensor TOut, Paddings TPaddings, ScalarOrVector TElem>
+void pad_impl(const TIn &input, TOut &output, const TPaddings &paddings,
+              const TElem &pad_value) {
+    constexpr auto rank = TIn::rank();
+    if constexpr (FixedDimension<std::tuple_element_t<
+                      rank - 1, typename TIn::shape_type>>) {
+        using element_type = typename TIn::element_type;
+        if (paddings[rank - 1][0] == 0 && paddings[rank - 1][1] == 0) {
+            auto domains = output.shape().template slice<0, rank - 1>();
+            auto input_strides = input.strides().template slice<0, rank - 1>();
+            auto output_strides =
+                output.strides().template slice<0, rank - 1>();
+            constexpr auto last_dim =
+                std::tuple_element_t<rank - 1, typename TIn::shape_type>{};
+
+            ntt::apply(domains, [&](auto out_index) {
+                bool dopad = false;
+                auto addr_output = output.elements().data() +
+                                   linear_offset(out_index, output_strides);
+
+                const auto in_index = generate_shape<rank - 1>([&](auto i) {
+                    auto in_dim = out_index[i] - paddings[i].before;
+                    if (in_dim < 0 || in_dim >= input.shape()[i]) {
+                        dopad = true;
+                    }
+                    return in_dim;
+                });
+                if (dopad) {
+                    alignas(sizeof(element_type))
+                        element_type pad_vals[last_dim]{};
+                    std::fill(std::begin(pad_vals), std::end(pad_vals),
+                              pad_value);
+                    ntt::u_unary(ntt::ops::copy<element_type>{}, pad_vals, 1,
+                                 addr_output, 1, last_dim);
+                } else {
+                    auto addr_input = input.elements().data() +
+                                      linear_offset(in_index, input_strides);
+                    ntt::u_unary(ntt::ops::copy<element_type>{}, addr_input, 1,
+                                 addr_output, 1, last_dim);
+                }
+            });
+        } else {
+            naive_apply(input, output, paddings, pad_value);
+        }
+    } else {
+        naive_apply(input, output, paddings, pad_value);
+    }
 }
 } // namespace pad_detail
 
@@ -51,9 +107,9 @@ void pad_impl(const TIn &input, TOut &output, const TPaddings &paddings,
  */
 template <Tensor TIn, class TOut, Paddings TPaddings,
           ScalarOrVector TElem = typename TIn::element_type>
-    requires(bool(TIn::rank() == TPaddings::rank()))
-void pad(const TIn &input, TOut &&output, const TPaddings &paddings,
-         const TElem &pad_value = {}) noexcept {
+requires(bool(TIn::rank() == TPaddings::rank())) void pad(
+    const TIn &input, TOut &&output, const TPaddings &paddings,
+    const TElem &pad_value = {}) noexcept {
     pad_detail::pad_impl(input, output, paddings, pad_value);
 }
 } // namespace nncase::ntt
